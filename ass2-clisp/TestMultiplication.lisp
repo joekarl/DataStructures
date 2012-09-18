f10a87ad14aabc73395f61489edd06ed2b586a61
Karl Kirch <kkirch@me.com>
2012-09-17 22:22:45 -0500

    Cleaned up README and added documentation
    Now has good documentation for the code

--- ass2-clisp/TestMultiplication.lisp
+++ ass2-clisp/TestMultiplication.lisp
@@ -2,31 +2,25 @@
 (load "./binary_list_manipulation.lisp")
 (load "./binary_list_math.lisp")
 
+;;function to print the usage information
 (defun print-usage()
   (print "Usage -- clisp TestMultiplication.lisp [<nTest>] [<n1>] [<n2>]
     nTest -- number of bits per muliplicant (must be <= 128)
-    n1 -- first multiplicant, in binary, <= nTest bits
-    n2 -- second multiplicant, in binary, <= nTest bits"))
-
-;;Find the next factor of 2 above a given number
-(defun normalize-to-factor-of-2 (n)
-  (let (next-factor)
-    (setf next-factor 1)
-    (loop
-      (if (>= next-factor n) (return))
-      (setf next-factor (ash next-factor 1)))
-    next-factor))
+    n1 -- first multiplicant, in binary, <= nTest bits ex: 10101011
+    n2 -- second multiplicant, in binary, <= nTest bits ex: 11001110"))
 
 ;;;Define vars needed to handle command line args
 (let (args nTest bl1 bl2 n1 n2 rtnVal maxSize)
   
   ;;parse command line args
+  ;;parse binary numbers into decimal to check for valid number sequences
   (setf args (command-line-args))
   (if (nth 0 args) (setf nTest (parse-integer (nth 0 args))))
   (if (nth 1 args) (setf n1 (parse-integer (nth 1 args) :radix 2)))
   (if (nth 2 args) (setf n2 (parse-integer (nth 2 args) :radix 2)))
 
   ;;if args weren't passed in, lets interactively ask the user for args...
+  ;;parse binary numbers into decimal to check for valid number sequences
   (if (not (= (list-length args) 3))
     (progn
       (print "Enter number of bits to use for multiplying numbers")
@@ -37,6 +31,8 @@
       (setf n2 (parse-integer (read-line) :radix 2))))
 
   ;;test to see if command line args are valid
+  ;;make sure nTest is valid
+  ;;make sure n1 and n2 are within the range of numbers that can be expresed in nTest bits
   (if 
     (or
       (null nTest)
@@ -44,17 +40,20 @@
       (not (<= n1 (bit-vector->integer (make-array nTest :initial-element 1))))
       (not (<= n2 (bit-vector->integer (make-array nTest :initial-element 1)))))
     (progn
+      ;;did not meet requirements, print usage statement and quit with error 1
       (print "Invalid Usage")
       (print-usage)
       (quit 1)))
   
   ;;args are good, lets do the multiplication now....
+  ;;but first coerce the converted n1/n2 values into a list of bits
   (setf bl1 (coerce (integer->bit-vector n1) 'list))
   (setf bl2 (coerce (integer->bit-vector n2) 'list))
 
   ;;do the multiplication with a normalized bit length 
   (setf rtnVal (b* nTest bl1 bl2))
   
-  ;;and print out what happened...
+  ;;and print out what happened in binary...
   (format t "~%In Binary..... ~A x ~A is ~B ~%" (nth 1 args) (nth 2 args) (bit-vector->integer rtnVal)) 
+  ;;print out the multiplication result in decimal for confirmation
   (format t "In Decimal.... ~A x ~A is ~A ~%" n1 n2 (bit-vector->integer rtnVal))) 
--- ass2-clisp/binary_list_math.lisp
+++ ass2-clisp/binary_list_math.lisp
@@ -1,8 +1,8 @@
 (load "./binary_list_manipulation.lisp")
 
-;;;Binary list shift left function (b<<)
-;;;l - list to shift
-;;;shiftNum - number of bits to shift left
+;;;Non destructive Binary list shift left function (b<<)
+;;;l - list to shift - not modified
+;;;shiftNum - number of bits to shift left - not modified
 ;;;Returns an array of nBits with the shifted number
 (defun b<< (l shiftNum) 
   (let (isZero)
@@ -30,7 +30,8 @@
             (setf temp (append () r))))
         r))))
 
-;;;Binary list addition function (b+)
+;;;Non destructive Binary list addition function (b+)
+;;;n1 and n2 are not modified 
 ;;;returns a list that has the added numbers from the passed in binary lists....
 (defun b+ (n1 n2)
   (let (carryBit r copyN1 copyN2 maxLength)
@@ -67,18 +68,19 @@
     (setf r (clean-list r))
     r))
 
-;;;Binary list multiplication function (b*)
-;;;nBits -- number of bits in numbers
-;;;n1 -- binary number list with no more bits than nBits 
-;;;n2 -- binary number list with no more bits than nBits 
-;;;returns a list full of binary numbers....
-
+;;;Non destructive Binary list multiplication function (b*)
+;;;nBits -- number of bits in numbers - not modified
+;;;n1 -- binary number list with no more bits than nBits - not modified
+;;;n2 -- binary number list with no more bits than nBits - not modified
+;;;returns a list full of binary numbers containing the result of the multiplication of the two numbers represented by the list n1 and n2....
 (defun b* (nBits n1 n2)
+  ;;in case this was used incorrectly, pad both lists so that they have nBits length
   (setf n1 (pad-list n1 nBits))
   (setf n2 (pad-list n2 nBits))
    
   ;;setup local variables
-  ;;m being half of nBits so we can split binary number in half
+  ;;m being half of nBits (ceiling) so we can split binary number in half
+  ;;n/2 being half of nBits (floor) so we can split binary number in half
   ;;xl being left half of n1
   ;;xr being right half of n1
   ;;yl being left half of n2
@@ -86,20 +88,24 @@
   ;;r being base case return
   (let (m xl xr yl yr r n/2)
     (setf n/2 (floor nBits 2))
-    ;;set xl to be left half of n1 by bit shifting to the right by m
+    
+    ;;set xl to be left half of n1
     (setf xl (subseq n1 0 n/2))
-    ;;set mask xl from n1 as xr
+    ;;set xl to be right half of n1
     (setf xr (subseq n1 n/2))
-    ;;set yl to be left half of n2 by bit shifting to the right by m
+    ;;set yl to be left half of n2
     (setf yl (subseq n2 0 n/2))
-    ;;set mask yl from n2 as yr
+    ;;set yl to be right half of n2
     (setf yr (subseq n2 n/2))
-    ;;calculate m
+    
+    ;;calculate m for later
     (setf m (- nBits n/2))
     
     (if
       (= nBits 1)
-      ;;if we have hit base case, return either 1 or 0 based on our multiplicants
+      ;;if we have hit base case (nBits == 1), return either 1 or 0 based on our multiplicants
+      ;;return a single element list with 1 if both multiplicants are 1
+      ;;return a single element list with 0 for all other cases 
       (progn
         (setf r (if (and (= (nth 0 (last n1)) 1) (= (nth 0 (last n2)) 1)) '(1) '(0)))
         r)
@@ -116,6 +122,7 @@
       ;;                   +
       ;;             (xr*yr)
       ;;
+      ;;and in code....
       (progn
         (b+ 
           (b+ 
