4bd410933b25752647273c2fe8622ee3ff032839
Karl Kirch <kkirch@me.com>
2012-10-03 13:26:14 -0500

    Added updated heap implementation with lgn comparisons

--- ass3-ruby/heap.rb
+++ ass3-ruby/heap.rb
@@ -1,14 +1,8 @@
 
-#Heap class that extends Array
+#Extension of Array class
 #Exposes a sort method to handle sorting of the heap
-class Heap < Array 
-  attr_accessor :heap_size
+class Array 
  
-  #inplace swap method (Hooray for ruby compound assignment) 
-  def swap!(a,b)
-    self[a], self[b] = self[b], self[a]
-  end
-  
   #Sort a heap in ascending order 
   #(as described via the text)
   #
@@ -18,7 +12,7 @@ class Heap < Array
   #inputs: none
   #
   #outputs: none 
-  def sort
+  def heapsort
     build_heap
     length.downto(2) do |i|
       swap! 0, i - 1
@@ -29,6 +23,13 @@ class Heap < Array
 
   #define private methods....
   private
+  
+  attr_accessor :heap_size
+  
+  #inplace swap method (Hooray for ruby compound assignment) 
+  def swap!(a,b)
+    self[a], self[b] = self[b], self[a]
+  end
 
   #Build the heap topology in place
   #(as described via the text)
@@ -47,37 +48,129 @@ class Heap < Array
   end
 
   #Heapify Downward
-  #(as described via the text)
-  #(or in the text, Max_Heapify)
   #
   #Ensures that index i obeys the heap topology
   #or it modifies the heap to make index i obey the heap topology
   #
-  #This method acts upon the heap's internal structure
-  #So this means it is destructive to the heap input
+  #Assumes that nodes under index i are currently in a heap topology ****** very important
   #
   #inputs:
   #i - index to check/modify
   #
   #outputs: none
   def heapify_downward(i)
-    l = left_child_index(i)
-    r = right_child_index(i)
+    #find the lowest node index that the node i could fall to
+    end_node_index = find_end_of_node_path(i)
     
-    if l <= @heap_size and self[l - 1] > self[i - 1] 
-      largest = l  
-    else 
-      largest = i
+    #stash the value of node i for later
+    start_node_value = self[i - 1] 
+
+    #search for the eventual final index where node i will fall
+    swap_node_index = search_path_for_insert_position(i, end_node_index, start_node_value)
+ 
+    #Move all parent nodes from the swap node index, up one level
+    traversing_node_index = swap_node_index 
+    swap_node_value = self[traversing_node_index - 1]
+    parent_value = self[parent_index(traversing_node_index) - 1]
+    while traversing_node_index != i 
+      traversing_node_index = parent_index(traversing_node_index)
+      self[traversing_node_index - 1] = swap_node_value
+      swap_node_value = parent_value
+      parent_value = self[parent_index(traversing_node_index) - 1]
+    end
+   
+    #finally set the swap node index value to be our initial value 
+    self[swap_node_index - 1] = start_node_value 
+  end
+
+  #binary search along a node path
+  #
+  #inputs: 
+  #start_node_index - index at the start of a path
+  #end_node_index - index at the end of a path
+  #key_value - value we are going to search for in the path
+  #
+  #outputs:
+  #the position of the key_value --OR-- in this case the position that the
+  #key value should be inserted at
+  def search_path_for_insert_position(start_node_index, end_node_index, key_value)
+   
+    if start_node_index >= end_node_index
+      return start_node_index
     end
     
-    if r <= @heap_size and self[r - 1] > self[largest - 1] 
-      largest = r  
+    if parent_index(end_node_index) == start_node_index
+      #if only two elements in the path
+      #use the end one as the midpoint
+      mid = end_node_index
+    else
+      #calculate the mid point of the path
+      mid_advances = 0.5 * Math.log(end_node_index) / Math.log(2)
+      mid = end_node_index
+      mid_advances.floor.downto(1) do |i|
+        mid = parent_index(mid)
+      end
     end
-    
-    if largest != i
-      swap! i - 1, largest - 1
-      heapify_downward largest
+
+    if self[mid - 1] > key_value
+      search_path_for_insert_position(next_child_in_path(start_node_index, end_node_index), end_node_index, key_value)
+    elsif self[mid - 1] < key_value
+      search_path_for_insert_position(start_node_index, parent_index(mid), key_value)
+    else
+      #we've found the insert position 
+      mid 
     end 
+  end
+
+  #find the index of the end node of the correct path for heapification
+  #
+  #inputs:
+  #i - the starting node
+  #
+  #outputs:
+  #the end of the supposed path
+  def find_end_of_node_path(i)
+    left_index = left_child_index(i)
+    right_index = right_child_index(i)
+   
+    if left_index > @heap_size && right_index > @heap_size
+      i
+    elsif right_index > @heap_size
+      left_index
+    else
+      max_child_index = -1
+      left_child_value = self[left_index - 1]
+      right_child_value = self[right_index - 1]
+      if right_child_value && left_child_value > right_child_value
+        max_child_index = left_index
+      else
+        max_child_index = right_index
+      end
+      find_end_of_node_path max_child_index
+    end
+  end
+
+  #find the next child in a given path
+  #
+  #inputs:
+  #start_node_index - start of path
+  #end_node_index - end of path
+  #
+  #output:
+  #the index of the next child in a given path
+  def next_child_in_path(start_node_index, end_node_index)
+    next_child = end_node_index
+    while parent_index(end_node_index) != start_node_index
+      end_node_index = parent_index(end_node_index)
+      next_child = end_node_index
+    end 
+    next_child
+  end
+
+  #returns the index of the parent of a heap node  
+  #(as described via the text)
+  def parent_index(i)
+    (i / 2).floor
   end
   
   #returns the index of the left child of a heap node
--- ass3-ruby/main.rb
+++ ass3-ruby/main.rb
@@ -1,10 +1,20 @@
 require "heap.rb"
 
-a = Heap.new [5,13,2,25,7,17,20,8,4]
+a = [5,13,2,25,7,17,20,8,4]
 
 puts "Array starts as #{a.inspect}"
 
-a.sort
+a.heapsort
+
+puts "And after sort #{a.inspect}"
+
+puts "------------------------------------------------------------------------------"
+
+a = [52,25,12,3,4,76,4,23,6,8,9,6,3,23,23,54,56,6,8,9,54,34,34,89,43,13,2,25,7,17,20,8,4]
+
+puts "Array starts as #{a.inspect}"
+
+a.heapsort
 
 puts "And after sort #{a.inspect}"
 
