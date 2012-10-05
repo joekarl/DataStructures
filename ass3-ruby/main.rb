33fca4eb3d24a63132a37a5a7916c12e082d2b47
Karl Kirch <kkirch@me.com>
2012-10-05 06:43:14 -0500

    Added better test case for heapsort

--- ass3-ruby/main.rb
+++ ass3-ruby/main.rb
@@ -1,4 +1,5 @@
 require "heap.rb"
+require 'test/unit'
 
 a = [5,13,2,25,7,17,20,8,4]
 
@@ -10,17 +11,31 @@ puts "And after sort #{a.inspect}"
 
 puts "------------------------------------------------------------------------------"
 
-a = [52,25,12,3,4,76,4,23,6,8,9,6,3,23,23,54,56,6,8,9,54,34,34,89,43,13,2,25,7,17,20,8,4,234,6,4,2,5,7,9,5,3,2,4,6,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,23,3,4,5,6,7,8,9]
+#a = [52,25,12,3,4,76,4,23,6,8,9,6,3,23,23,54,56,6,8,9,54,34,34,89,43,13,2,25,7,17,20,8,4,234,6,4,2,5,7,9,5,3,2,4,6,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,23,3,4,5,6,7,8,9]
 #a = [52,25,12,3,4,76,4,23,6,8,9,6,3,23,23,54,56,6,8,9,54,34,34,89,43,13,2,25,7,17,20,8,4]
 #a = [1,2,3,4,5,6,7,1,2,3,4,5,6,7,8,9]
 #a = [1,2,3,4,5,6,7,1,2,3,4,5,6,7,8,9]
 #a = [4,9,2,1]
 
-puts "Array starts as #{a.inspect}"
+class TestHeapsort < Test::Unit::TestCase
+  def test_heapsort
+    (0..1000).each do |x|
+      puts "Testing permutation #{x + 1}"
+      a = []
+      seed = srand()
+      (0..100).each {|i| a[i] = rand(seed) % 1000}
 
-#a.heap_size = a.length
-#a.heapify_downward 1
-a.heapsort
+      copy_a = a
+      a.heapsort
 
-puts "And after sort #{a.inspect}"
+      failed = false
+      last = a[0]
+      a.each do |val|
+        failed = true if val < last
+        last = val
+      end
 
+      assert !failed, "Failed to sort array #{copy_a} and ended with #{a}"
+    end
+  end
+end
